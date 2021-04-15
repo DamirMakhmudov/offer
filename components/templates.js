cced0ac7d553434f743cf950f30319975b716c73
Damir <d.makhmudov@kazan.csoft.ru>
2021-04-15 11:40:11 +0300

    one

---
+++ components/templates.js
@@ -0,0 +1,22 @@
+app.component('templates',{
+  name: 'templates',
+  template:
+  /*html*/
+  `
+  
+  `
+  ,
+  props:{
+    temp:{
+      type: String
+    }
+  },
+  setup(props){
+    
+    return{
+
+    }
+  }
+
+
+})
