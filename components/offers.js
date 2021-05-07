4b84a1dd50d276b9b74e6440eb7da0d0ede5f1b8
Damir <d.makhmudov@kazan.csoft.ru>
2021-05-07 11:13:58 +0300

    some changes

--- components/offers.js
+++ components/offers.js
@@ -6,6 +6,7 @@ app.component('offers',{
   <div class="q-ma-md">
     <div class="full-width row justify-between">
       <q-select v-model="selectedoffer.val" :options="options" label="Выберите шаблон" class="col" style="overflow: auto;"/>
+      <q-btn color="positive" icon="print" label="Печать" @click="printIt(selectedoffer.val)" style="overflow: auto;max-height:4em"/>
       <q-btn color="primary" icon="save" label="Сохранить" @click="saveIt(selectedoffer.val)" style="overflow: auto;max-height:4em"/>
     </div>
   </div>
@@ -15,14 +16,24 @@ app.component('offers',{
     const $q = useQuasar()
 
     function saveIt(v){
-      saveDataJS(v.label);
-      $q.loading.show();
+      // saveDataJS(v.label);
+      $q.loading.show({
+        message: 'Секунду. Я сохраняю данные и сам закрою диалог...',
+      });
+    }
+
+    function printIt(v){
+      // printJS(v.label);
+      $q.loading.show({
+        message: 'Печатаю компредложение. Нужно немного подождать...',
+      });
     }
     
     return{
       selectedoffer: ref(model.selectedOffer),
       options: view.filterOffers,
-      saveIt
+      saveIt,
+      printIt
     }
   }
 })
