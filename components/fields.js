1025b625ed30e5b15afcddbde88a27f66b1a231a
Damir <d.makhmudov@kazan.csoft.ru>
2022-01-27 15:40:49 +0300

    added fields and selectedrows

--- components/fields.js
+++ components/fields.js
@@ -1,26 +1,8 @@
-app.component('titleblock', {
-  name: 'titleblock',
+app.component('fields', {
+  name: 'fields',
   template:
-    /*html*/
-    `
-  <!-- Выбранные позиции -->
-  <q-expansion-item
-    expand-separator
-    icon="view_list"
-    label="Выбранные позиции"
-    default-opened
-  >
-    <div class="q-ma-sm" style="overflow: auto">
-      <q-list dense bordered separator class="rounded-borders">
-        <q-item v-for="item in selectedc.val" clickable v-ripple>
-          <q-item-section class="text-caption">
-          {{item.id}} {{item.name}}
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </div>
-  </q-expansion-item>
-
+  /*html*/
+  `
   <!-- Данные физлица -->
   <q-expansion-item
     expand-separator
